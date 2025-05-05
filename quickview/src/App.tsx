20c4a5b918d3c1cf3390f98a8dab974bb8d738b4
Jorge Montero <jmon14@gmail.com>
2025-05-05 11:42:22 +0200

    refactor: extract routing logic into AppRoutes component

--- quickview/src/App.tsx
+++ quickview/src/App.tsx
@@ -1,32 +1,18 @@
 // External libraries
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import Cookies from 'js-cookie';
 
 // Components
-import ResetPassword from './components/organisms/reset-password';
-import NewPassword from './components/organisms/new-password';
-import Register from './components/organisms/register';
-import Validate from './components/organisms/validate';
-import Uploader from './components/organisms/uploader';
 import Loading from './components/molecules/loading';
-import Files from './components/organisms/files';
-import Login from './components/organisms/login';
-import Auth from './components/pages/auth';
-import Home from './components/pages/home';
 
 // Routes
-import { AuthProtected } from './routes/auth-protected';
+import { AppRoutes } from './routes/app-routes';
 
 // Store
 import { useAppSelector } from './hooks/store-hooks';
 
 // Utils
 import { darkTheme, lightTheme } from './theme/theme';
-import RangeBuilder from './components/organisms/range-builder';
-
-// Props passed down to auth protected route
-const refreshCookie = Cookies.get('RefreshExist');
 
 const App = () => {
   const { mode } = useAppSelector((state) => state.theme);
@@ -36,40 +22,7 @@ const App = () => {
       <CssBaseline />
       <Loading />
       <BrowserRouter>
-        <Routes>
-          <Route path="auth" element={<Auth />}>
-            <Route
-              element={
-                <AuthProtected
-                  refreshCookie={refreshCookie}
-                  shouldBeLogged={false}
-                  redirectPath="/"
-                />
-              }
-            >
-              <Route path="new-password" element={<NewPassword />} />
-              <Route path="reset" element={<ResetPassword />} />
-              <Route path="register" element={<Register />} />
-              <Route path="login" element={<Login />} />
-            </Route>
-            <Route path="validate" element={<Validate />} />
-          </Route>
-          <Route
-            element={
-              <AuthProtected
-                refreshCookie={refreshCookie}
-                shouldBeLogged={true}
-                redirectPath="auth/login"
-              />
-            }
-          >
-            <Route path="" element={<Home />}>
-              <Route path="upload" element={<Uploader />} />
-              <Route path="dashboard" element={<Files />} />
-              <Route path="range" element={<RangeBuilder />} />
-            </Route>
-          </Route>
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </ThemeProvider>
   );
---
+++ quickview/src/routes/app-routes.tsx
@@ -0,0 +1,60 @@
+// External libraries
+import { Route, Routes } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
+// Components
+import ResetPassword from '../components/organisms/reset-password';
+import NewPassword from '../components/organisms/new-password';
+import Register from '../components/organisms/register';
+import Validate from '../components/organisms/validate';
+import Uploader from '../components/organisms/uploader';
+import Files from '../components/organisms/files';
+import Login from '../components/organisms/login';
+import Auth from '../components/pages/auth';
+import Home from '../components/pages/home';
+import RangeBuilder from '../components/organisms/range-builder';
+
+// Routes
+import { AuthProtected } from './auth-protected';
+
+// Props passed down to auth protected route
+const refreshCookie = Cookies.get('RefreshExist');
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="auth" element={<Auth />}>
+        <Route
+          element={
+            <AuthProtected
+              refreshCookie={refreshCookie}
+              shouldBeLogged={false}
+              redirectPath="/"
+            />
+          }
+        >
+          <Route path="new-password" element={<NewPassword />} />
+          <Route path="reset" element={<ResetPassword />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+        <Route path="validate" element={<Validate />} />
+      </Route>
+      <Route
+        element={
+          <AuthProtected
+            refreshCookie={refreshCookie}
+            shouldBeLogged={true}
+            redirectPath="auth/login"
+          />
+        }
+      >
+        <Route path="" element={<Home />}>
+          <Route path="upload" element={<Uploader />} />
+          <Route path="dashboard" element={<Files />} />
+          <Route path="range" element={<RangeBuilder />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+}; 
