78cfd3baafdc74d48a01609175f93aaaff5f8940
Jorge Montero <jamonra14@gmail.com>
2025-06-25 09:26:30 +0200

    feat: adding escaping once again

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -53,15 +53,22 @@ aws ssm send-command \
     --document-name "AWS-RunShellScript" \
     --parameters "commands=[
         'cd ~/texas',
-        'POSTGRES_USER=$(aws ssm get-parameter --name "/texas/ultron/POSTGRES_USER" --query "Parameter.Value" --output text)',
-        'POSTGRES_PASSWORD=$(aws ssm get-parameter --name "/texas/ultron/POSTGRES_PASSWORD" --with-decryption --query "Parameter.Value" --output text)',
-        'MONGO_USER=$(aws ssm get-parameter --name "/vision/mongodb/MONGO_USER" --query "Parameter.Value" --output text)',
-        'MONGO_PASSWORD=$(aws ssm get-parameter --name "/vision/mongodb/MONGO_PASSWORD" --with-decryption --query "Parameter.Value" --output text)',
-        'echo "POSTGRES_USER=$POSTGRES_USER" > .env',
-        'echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env',
-        'echo "MONGO_USER=$MONGO_USER" >> .env',
-        'echo "MONGO_PASSWORD=$MONGO_PASSWORD" >> .env',
-        'echo "Environment variables configured"'
+        'echo \"Starting environment variable setup...\"',
+        'POSTGRES_USER=\$(aws ssm get-parameter --name \"/texas/ultron/POSTGRES_USER\" --query \"Parameter.Value\" --output text)',
+        'echo \"POSTGRES_USER retrieved: \$POSTGRES_USER\"',
+        'POSTGRES_PASSWORD=\$(aws ssm get-parameter --name \"/texas/ultron/POSTGRES_PASSWORD\" --with-decryption --query \"Parameter.Value\" --output text)',
+        'echo \"POSTGRES_PASSWORD retrieved: [HIDDEN]\"',
+        'MONGO_USER=\$(aws ssm get-parameter --name \"/vision/mongodb/MONGO_USER\" --query \"Parameter.Value\" --output text)',
+        'echo \"MONGO_USER retrieved: \$MONGO_USER\"',
+        'MONGO_PASSWORD=\$(aws ssm get-parameter --name \"/vision/mongodb/MONGO_PASSWORD\" --with-decryption --query \"Parameter.Value\" --output text)',
+        'echo \"MONGO_PASSWORD retrieved: [HIDDEN]\"',
+        'echo \"POSTGRES_USER=\$POSTGRES_USER\" > .env',
+        'echo \"POSTGRES_PASSWORD=\$POSTGRES_PASSWORD\" >> .env',
+        'echo \"MONGO_USER=\$MONGO_USER\" >> .env',
+        'echo \"MONGO_PASSWORD=\$MONGO_PASSWORD\" >> .env',
+        'echo \"Environment variables configured successfully\"',
+        'echo \"Contents of .env file:\"',
+        'cat .env | sed \"s/=.*/=***/\"'
     ]" \
     --output text
 
