da1af4ec5e9df4f58ee5b810ce9c361e49ded1d9
Jorge Montero <jamonra14@gmail.com>
2025-07-11 18:20:09 +0200

    feat: add ecr configuration

---
+++ infrastructure/aws/ecr.tf
@@ -0,0 +1,45 @@
+# ECR Repositories for Docker images
+resource "aws_ecr_repository" "ultron" {
+  name = "texas-ultron"
+  
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "texas-ultron"
+    Environment = "production"
+    Project     = "texas"
+    ManagedBy   = "terraform"
+  }
+}
+
+resource "aws_ecr_repository" "vision" {
+  name = "texas-vision"
+  
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "texas-vision"
+    Environment = "production"
+    Project     = "texas"
+    ManagedBy   = "terraform"
+  }
+}
+
+resource "aws_ecr_repository" "quickview" {
+  name = "texas-quickview"
+  
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "texas-quickview"
+    Environment = "production"
+    Project     = "texas"
+    ManagedBy   = "terraform"
+  }
+} 
